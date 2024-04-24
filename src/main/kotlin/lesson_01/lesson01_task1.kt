36d3bfde173eac0977425551c88e1f96912179d0
senkamd <irina.miletic1@gmail.com>
2024-04-24 20:44:01 +0200

    Empty files for first lessons created

--- src/main/kotlin/lesson_01/lesson01_task1.kt
+++ src/main/kotlin/lesson_01/lesson01_task1.kt
@@ -3,6 +3,6 @@ package org.example.lesson_01
 class lesson01_task1 {
 
     var numberOfOrders: Int = 75
-    val gratitudeText: String = "Благодарим за покупку!"
+    val gratitudeText: String = "Спасибо за покупку!"
 
 }
