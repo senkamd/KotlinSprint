4cc919ff639f9b3b1575d950d193e9997aa0f92c
senkamd <irina.miletic1@gmail.com>
2024-04-24 22:19:36 +0200

    KS-1-2 second task is done

---
+++ src/main/kotlin/lesson_02/lesson01_task2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_02
+
+fun main() {
+
+    var numberOfOrders: Int = 75
+    val gratitudeText: String = "Благодарим за покупку!"
+
+    println(numberOfOrders)
+    println(gratitudeText)
+
+    var numberOfEmployee: Int = 2000
+
+//    println(numberOfEmployee)
+
+    numberOfEmployee = 1999
+
+    println(numberOfEmployee)
+}
+
