9ea5dd2199da71978bc514c96aa73db995eb935f
senkamd <irina.miletic1@gmail.com>
2024-04-25 13:51:05 +0200

    KS-2-1 task is done

---
+++ src/main/kotlin/lesson_02/Lesson02_task1.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_02
+
+fun main(){
+
+    val student1: Int = 3
+    val student2: Int = 4
+    val student3: Int = 3
+    val student4: Int = 5
+
+    var averageScore = (student1 + student2 + student3 + student4) / 4.0
+
+    println(averageScore)
+}
+
